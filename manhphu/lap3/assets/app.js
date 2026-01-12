5014b90451b15b31a69243b917c1e699bfe3223a
Manh12phu <143483525+Manh12phu@users.noreply.github.com>
2026-01-12 10:21:17 +0700

    Add files via upload

---
+++ manhphu/lap3/assets/app.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const toggleBtn = document.getElementById("toggleBtn");
+  
+    nut.onclick = function() {
+      document.body.classList.toggle("dark");
+    };
+  });
+  
